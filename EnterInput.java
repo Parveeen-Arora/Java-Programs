76a7d4907a2a079a09d888490640ce0e1dc502ae
Parveeen-Arora <parveenarora291@gmail.com>
2024-10-12 10:52:48 +0530

    IP_RunTime

---
+++ EnterInput.java
@@ -0,0 +1,15 @@
+package IP_RunTime;
+
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class EnterInput 
+{
+	public static void main(String[] args) 
+	{	
+		System.out.println("Enter the value of a ");
+		Scanner e1=new Scanner(System.in);
+		int a=	e1.nextInt();
+		System.out.println("Value of a is "+a);
+	}
+}	
