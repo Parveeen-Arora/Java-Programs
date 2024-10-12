491c47879ef1162b304ccdff20fc227a700c8095
Parveeen-Arora <parveenarora291@gmail.com>
2024-10-12 10:47:10 +0530

    IP_RunTime

---
+++ Area_of_Square.java
@@ -0,0 +1,16 @@
+package IP_RunTime;
+
+import java.util.Scanner;
+
+public class Area_of_Square 
+{
+	public static void main(String[] args) 
+	{
+		Scanner a1=new Scanner(System.in);
+		System.out.println("Enter the value of a");
+		int a=a1.nextInt();
+		int res=a*a;
+		System.out.println("Area of square is "+res);
+	}
+}
+	
