fe7ae587ca2fa1af18cddc7f1546741e3ca55a62
Parveeen-Arora <parveenarora291@gmail.com>
2024-10-22 10:55:09 +0530

    Hello

---
+++ Tc1.java
@@ -0,0 +1,15 @@
+package InheritanceDemo;
+
+public class Tc1 extends Screenshots
+{
+	Tc1()
+	{
+		super();
+		System.out.println("Executing the test cases");
+	}
+	public static void main(String[] args) 
+	{
+		new Tc1();
+	}
+
+}
